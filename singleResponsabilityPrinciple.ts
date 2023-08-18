d233b8ad49bbff55a35684f520f6a5582cad0fdb
Fábio Rodrigues Fonseca <ffonseca1985@gmail.com>
2023-08-18 10:48:35 -0300

    Update singleResponsabilityPrinciple.ts

--- singleResponsabilityPrinciple.ts
+++ singleResponsabilityPrinciple.ts
@@ -1,3 +1,3 @@
-O Princípio da Responsabilidade Única é um conceito da programação e do design de software que faz parte dos princípios SOLID, que foram introduzidos por Robert C. Martin. 
-O princípio da Responsabilidade Única (Single Responsibility Principle - SRP) estabelece que uma classe deve ter apenas um motivo para ser modificada. 
-Em outras palavras, uma classe deve ter apenas uma única responsabilidade.
+- O Princípio da Responsabilidade Única é um conceito da programação e do design de software que faz parte dos princípios SOLID, que foram introduzidos por Robert C. Martin. 
+- O princípio da Responsabilidade Única (Single Responsibility Principle - SRP) estabelece que uma classe deve ter apenas um motivo para ser modificada. 
+- Em outras palavras, uma classe deve ter apenas uma única responsabilidade.
